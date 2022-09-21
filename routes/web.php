146490b79e4e0917e9c83e3ac8a4071028eea4e8
Anando Rahul <anandorahul@gmail.com>
2022-09-21 09:29:50 +0700

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -21,24 +21,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('galeries/')->name('galeries.')->group(function () {
-    Route::get('/', [GaleriesController::class, 'index'])->name('index');
-    Route::get('create', [GaleriesController::class, 'create'])->name('create');
-    Route::post('store', [GaleriesController::class, 'store'])->name('store');
-    Route::delete('delete/{galeries:id}', [GaleriesController::class, 'delete'])->name('delete');
-    Route::get('edit/{galeries:id}', [GaleriesController::class, 'edit'])->name('edit');
-    Route::put('update/{galeries:id}', [GaleriesController::class, 'update'])->name('update');
-
-});
-
-Route::prefix('posts/')->name('posts.')->group(function () {
-    Route::get('/', [PostController::class, 'index'])->name('index');
-    Route::get('create', [PostController::class, 'create'])->name('create');
-    Route::post('store', [PostController::class, 'store'])->name('store');
-    Route::delete('delete/{posts:id}', [PostController::class, 'delete'])->name('delete');
-    Route::get('edit/{posts:id}', [PostController::class, 'edit'])->name('edit');
-    Route::put('update/{posts:id}', [PostController::class, 'update'])->name('update');
-
-});
-
 
