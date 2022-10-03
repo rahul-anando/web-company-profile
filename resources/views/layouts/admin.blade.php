e0e6993166ee5a6fd78f06f24928a2f740b2d4ca
Anando Rahul <anandorahul@gmail.com>
2022-10-03 14:29:20 +0700

    Change Users.Index View to DataTables

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,6 +8,7 @@
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 
   <!-- CSS Libraries -->
   <link rel="stylesheet" href="{{ asset('node_modules/bootstrap-social/bootstrap-social.css') }}">
@@ -49,6 +50,12 @@
   <script src="{{ asset('assets/js/stisla.js') }}"></script>
 
   <!-- JS Libraies -->
+  <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+  <script>
+    $(document).ready( function () {
+        $('#myTable').DataTable();
+    } );
+  </script>
 
   <!-- Template JS File -->
   <script src="{{ asset('assets/js/scripts.js') }}"></script>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -11,7 +11,7 @@
                     @endsection
                     <h4>Management User</h4>
                 </div>
-                <div class="card-body">
+                {{-- <div class="card-body">
                     <a href="{{ route('users.create') }}" class="btn btn-primary float-right mb-3">Tambah Data</a>
                     <div class="table-responsive">
                         <table class="table table-bordered table-md">
@@ -39,30 +39,47 @@
                                         </form>
                                     </td>
                                 </tr>
-
-                                </tr>
                             @endforeach
                         </table>
                     </div>
-                </div>
-                <div class="card-footer text-right">
-                    <nav class="d-inline-block">
-                        <ul class="pagination mb-0">
-                            <li class="page-item disabled">
-                                <a class="page-link" href="#" tabindex="-1"><i
-                                        class="fas fa-chevron-left"></i></a>
-                            </li>
-                            <li class="page-item active"><a class="page-link" href="#">1 <span
-                                        class="sr-only">(current)</span></a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">2</a>
-                            </li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#"><i class="fas fa-chevron-right"></i></a>
-                            </li>
-                        </ul>
-                    </nav>
+                </div> --}}
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <a href="{{ route('users.create') }}" class="btn btn-primary float-left mb-4">Tambah Data</a>
+                            <table id="myTable" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php $no = 1; @endphp
+                                    @foreach ($users as $user)
+                                        <tr>
+                                            <td>{{ $no++ }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td class="text-right">
+                                                <a class="btn btn-outline-success me-2 mb-2"
+                                                    href="{{ route('users.edit', $user->id) }}">Edit</a>
+
+                                                <form action="{{ route('users.delete', $user->id) }}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button class="btn btn-outline-danger" type="submit"
+                                                        onclick="return confirm('Yakin mau dihapus qaqa?')">Hapus</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
